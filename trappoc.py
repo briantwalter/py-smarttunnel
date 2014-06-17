9dd70b306e98cfe66874864a52d64edffb0afd11
briantwalter <walter.brian@gmail.com>
2014-06-16 22:58:34 -0700

    .01 precision

--- trappoc.py
+++ trappoc.py
@@ -15,7 +15,7 @@ from datetime import datetime
 # vars
 entr = 23		# GPIO for entrance sensor
 exit = 24		# GPIO for exit sensor
-loop = .05		# interval to check for values
+loop = .01		# interval to check for values
 dist = 11.5		# distance between sensors in inches
 prec = 3		# precision in digits to trip on
 mphi = 0.0568181818	# 1 inch per second = 0.0568181818 miles per hour
